b6f1bab782b8838209a1ddfb577cfe4cb72d1958
Mike Thomas <Mike Thomas>
2021-03-05 12:20:09 +1000

    Old build state.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,33 @@
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE FlexibleInstances, OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Lib
-    ( someFunc
+    ( aesonTest,
+      parseReferers,
+      inputData,
+      doIt,
+      Referer
     ) where
 
 import Data.Aeson
 import Data.Aeson.Types
+import Data.Either.Unwrap (isLeft, fromLeft, fromRight)
 import qualified Data.HashMap.Strict as HM
+import qualified Data.ByteString.Lazy.Char8 as DBSLC8
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+aesonTest :: IO ()
+aesonTest = putStrLn inputData
+
+inputData :: String
+inputData =
+   "{\
+    \\"website1.com\":\n\
+    \  {\"/page1\": 3,\n\
+    \   \"/page2\": 4}\n\
+    \,\"website2.com\":\n\
+    \  {\"/page\": 10}\n\
+    \}"
 
 data Referer = Referer {
   domain       :: String,
@@ -26,3 +43,24 @@ parseReferers p =
   HM.toList <$>
   -- Parse our data into a HashMap String (HashMap String Int).
   parseJSON p
+
+
+-- readMapFromString :: String -> Map
+-- readMapFromString candidateMap =
+--     let
+--         eitherMap = eitherDecode (DBSLC8.pack candidateMap) :: (Either String Map)
+--     in
+--         if isLeft eitherMap 
+--             then 
+--                 Map { MapDefinitions.map = [] }
+--             else
+--                 fromRight eitherMap
+
+doIt =
+  let
+    value = eitherDecode $ DBSLC8.pack inputData :: Either String Value
+    parsed = 
+      if isLeft value then error $ fromLeft value
+      else parseReferers $ fromRight value
+  in
+    parsed
